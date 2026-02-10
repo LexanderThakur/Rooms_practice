33d90eaafd44b929f2086076f48772f2c5b91925
Lexander <lexanderthakur3@gmail.com>
2026-02-10 16:29:33 +0530

    create room and get all rooms view done

--- backend/base/room_views.py
+++ backend/base/room_views.py
@@ -19,11 +19,41 @@ from rest_framework import status
 User= get_user_model()
 
 from .models import Room,RoomMembership
+from .serializers import (
+    RoomSerializer,
+    RoomCreateSerializer,
+    RoomMembershipSerializer,
+    RoomMembershipCreateSerializer
+)
 
 @api_view(['POST'])
 @authentication_classes([customjwt])
 @permission_classes([IsAuthenticated])
 def create_room(request):
+
+    serializer = RoomCreateSerializer(data=request.data)
+
+    if serializer.is_valid(raise_exception=True):
+        serializer.save(owner=request.user)
+
+    return Response({"message":"success"},status=201)
+
+
+
+@api_view(['GET'])
+@authentication_classes([])
+@permission_classes([AllowAny])
+def get_all_rooms(request):
+
+    rooms_qs= Rooms.objects.all()
+    data=[]
+
+    for room in rooms_qs:
+        serializer=RoomSerializer(room)
+        data.append(serializer.data)
+
+    return Response({"message":data},status=200)
+
 
     
 
--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path 
 
 from . import views
+from . import room_views
 urlpatterns=[
     path("register/",views.register),
     path("login/",views.login),
     path("me/",views.me),
+    path("create/",room_views.create_room),
+    path('get/',room_views.get_all_rooms),
 ]
